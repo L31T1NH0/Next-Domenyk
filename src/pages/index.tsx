39bc27a8d46cceacccbce6bdbd0cc2cfb5b3154a
L31T1NH0 <oLeiteG@hotmail.com>
2023-03-29 02:07:03 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import data from '../data.json'
+import foto from '../image/domenyk.png'
 
 function LinkCard({ 
   href, 
@@ -23,7 +24,7 @@ function LinkCard({
             <Image 
             className='rounded-sm'
             alt={title}
-            src={image}
+            src={foto}
             width={40}
             height={40}
           />
@@ -42,7 +43,7 @@ export default function Home() {
   return(
     <div className="flex items-center flex-col mx-auto w-full 
     justify-center mt-16 px-8">
-      <Image 
+      <img
         className="rounded-full"
         alt={data.name}
         src={data.avatar}
