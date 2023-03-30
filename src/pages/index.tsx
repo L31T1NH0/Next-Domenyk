74e89619644c1f1d60615e53ea784e6016ab860b
L31T1NH0 <oLeiteG@hotmail.com>
2023-03-30 20:34:37 -0300

    Formatacao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,35 @@
-import Image from 'next/image'
-import data from '../data.json'
-import foto from '../image/insta.png'
+import Image from 'next/image';
+import data from '../data.json';
+import instaimg from '../image/insta.png';
+import photo from '../image/domenyk.png'
 
-function LinkCard({ 
-  href, 
-  title, 
-  image
-}: { 
+function LinkCard({
+  href,
+  title,
+  image,
+}: {
   href: string;
-   title: string;
-    image?: string 
-  }) {
+  title: string;
+  image?: string
+}) {
   return (
     <a
       href={href}
-      className="flex items-center p-1 w-full rounded-md
-      hover:scale-105 transition-all bg-gray-100 border
-    border-gray-300 mb-3 max-w-2xl"
+      className="flex items-center p-1 w-full rounded-md hover:scale-105 transition-all bg-gray-100 border border-gray-300 mb-3 max-w-2xl"
     >
       <div className="flex text-center w-full">
-        <div className='w-10 h-10'>
-        {image && (
+        <div className="w-10 h-10">
+          {image && (
             <Image
-            className='rounded-sm'
-            alt={title}
-            src={foto}
-            width={40}
-            height={40}
-          />
-        )}
+              className="rounded-sm"
+              alt={title}
+              src={instaimg}
+              width={40}
+              height={40}
+            />
+          )}
         </div>
-        <h2 className="flex font-semibold justify-center items-center
-        w-full text-gray-800 -ml-10">
+        <h2 className="flex font-semibold justify-center items-center w-full text-gray-800 -ml-10">
           {title}
         </h2>
       </div>
@@ -40,31 +38,46 @@ function LinkCard({
 }
 
 export default function Home() {
-  return(
-    <div className="flex items-center flex-col mx-auto w-full 
-    justify-center mt-16 px-8">
-      <img
-        className="rounded-full"
-        alt={data.name}
-        src={data.avatar}
-        width={96}
-        height={96}
-      />
-      <h1 className="font-bold mt-4 mb-8 text-xl text-white">{data.name}</h1>
+  return (
+    <div className="flex items-center flex-col mx-auto w-full justify-center mt-16 px-8">
+        <Image
+          className="rounded-full"
+          alt={data.name}
+          src={photo}
+          width={96}
+          height={96}
+        /> 
+      <h1 
+        className="font-bold mt-2 mb-2 text-xl text-black">
+        {data.name}
+      </h1>
+      <p 
+        className='text-sm max-w-2xl font-semibold text-center mb-2'>
+        {data.bio}
+      </p>
       {data.socials.map((social) => (
         <LinkCard key={social.href} {...social} />
       ))}
-      <div className='w-full h-full'>
-        <div className='justify-center items-center m-auto hover:scale-105 transition-all max-w-2xl'>
-          <form className='flex flex-col w-full'>
-              <input type="text" id="first" className='w-full h-12 p-1  px-4 rounded-md outline-none rounded-b-none bg-gray-100' maxLength={62} placeholder='Insira seu Nome' />
-              <textarea id="last" className='h-48 p-1 max-w-2xl px-4 rounded-md outline-none rounded-t-none resize-none bg-gray-100' maxLength={372} placeholder='Deixe sua Mensagem aqui :)' />
+      <div className="w-full h-full">
+        <div className="justify-center items-center m-auto hover:scale-105 transition-all max-w-2xl">
+          <form className="flex flex-col w-full mb-16">
+            <input
+              type="text"
+              id='nome'
+              className="w-full h-12 p-1 px-4 rounded-md outline-none rounded-b-none bg-gray-100 border border-gray-300"
+              maxLength={62}
+              placeholder="Insira seu Nome"
+            />
+            <textarea
+              id='mensagem'
+              className="h-48 p-1 max-w-2xl px-4 rounded-md outline-none rounded-t-none resize-none bg-gray-100 border border-gray-300"
+              maxLength={372}
+              placeholder="Deixe sua Mensagem aqui :)"
+            />
+            {/* adicionar um button */}
           </form>
-          <button type="submit" className='bg-black text-white p-1 rounded-md absolute flex '>Submit</button>
         </div>
-        
       </div>
     </div>
   );
 }
-
